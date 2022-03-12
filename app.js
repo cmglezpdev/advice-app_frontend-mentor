1b41629f5c41b6a843022d36256af7840016194e
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-03-12 17:55:47 -0500

    error message when api query fails

--- app.js
+++ app.js
@@ -6,17 +6,25 @@ document.addEventListener("DOMContentLoaded", () => {
 
 let title = document.querySelector(".card__title");
 let phrase = document.querySelector(".card__phrase");
-
+let btn_new_advice = document.querySelector(".card__title");
 
 const getAdvice = async () => {
     const url = "https://api.adviceslip.com/advice";
 
     let result = await fetch( url )
                     .then(advice => advice.json())
-                    .catch(error => console.log(error));
+                    .catch(() => {
+                        title.textContent = `ADVICE #0`;
+                        phrase.textContent = `No se puede establecer conexión con la API.`;
+                    });
 
     const { slip:{ advice, id } } = result;
 
     title.textContent = `ADVICE #${id}`;
     phrase.textContent = `${advice}`;
 }
+
+
+document.addEventListener("click", () => {
+    getAdvice();
+})
